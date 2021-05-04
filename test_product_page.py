2753269baeb364259230360080fc7b9dfba48872
dmitriy.durovich@samgau.com <dmitriy.durovich@samgau.com>
2021-05-04 11:32:19 +0600

    Добавлен код к шагу 8 модуля 4.3

--- test_product_page.py
+++ test_product_page.py
@@ -22,13 +22,13 @@ def test_add_bascket_product(browser, link):
     page.should_be_message_adding_bascket()
     page.should_be_message_price_basket()
 
-@pytest.mark.parametrize('link', [pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0", marks=pytest.mark.smoke)])
+@pytest.mark.parametrize('link', [pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0", marks=pytest.mark.regress)])
 def test_guest_cant_see_success_message(browser, link):
     page=ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
 
-@pytest.mark.parametrize('link', [pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0", marks=pytest.mark.smoke)])
+@pytest.mark.parametrize('link', [pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0", marks=pytest.mark.regress)])
 def test_message_disappeared_after_adding_product_to_basket(browser, link):
     page=ProductPage(browser, link)
     page.open()
@@ -36,10 +36,25 @@ def test_message_disappeared_after_adding_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()
     page.disappearing_success_message()
 
-@pytest.mark.parametrize('link', [pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0", marks=pytest.mark.smoke)])
+@pytest.mark.parametrize('link', [pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0", marks=pytest.mark.regress)])
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser, link):
     page=ProductPage(browser, link)
     page.open()
     page.add_product_button()
     page.solve_quiz_and_get_code()
     page.should_not_be_success_message()
+
+@pytest.mark.regress
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+@pytest.mark.smoke
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page.should_be_login_link()
